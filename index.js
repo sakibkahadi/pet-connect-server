dee338442e617b67018ee099aa6c7e1a112bd5f8
sakib kahadi <sakibkahadi72844@gamil.com>
2023-11-25 16:27:14 +0600

    added jwt token and verify it

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
@@ -31,15 +32,42 @@ async function run() {
     const petsCategoryCollection = client.db("petConnectDb").collection("petsCategory");
     const usersCollection = client.db("petConnectDb").collection("users");
 
-    app.get('/petsCategory', async (req, res) => {
+    //middle wares
+    const verifyToken = (req,res,next)=>{
+      // console.log('inside verify token', req.headers.authorization)
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'unauthorized access'})
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+          return res.status(401).send({message: 'unauthorized access'})
 
+        }
+        req.decoded = decoded;
+        next();
+      })
+     
+    }
+
+
+
+    //jwt related api
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token});
+    })
+
+    //category related api
+    app.get('/petsCategory', async (req, res) => {
       const result = await petsCategoryCollection.find().toArray()
       res.send(result)
     })
 
 
     // users related api
-     app.get('/users' ,async(req,res)=>{
+     app.get('/users',verifyToken ,async(req,res)=>{
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
