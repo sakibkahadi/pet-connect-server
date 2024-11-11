a20fdb983ddc162ec145e3c1700d1f1339b2ad96
sakib kahadi <sakibkahadi72844@gamil.com>
2024-11-12 01:57:38 +0600

    fixed bug

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173', 
+    origin: ['https://pet-connect-e4ef9.web.app','http://localhost:5173'], 
     methods: ['GET', 'POST', 'PATCH', 'PUT','DELETE'], 
     credentials: true, 
   })
